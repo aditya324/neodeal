50c030bf93ab63fe039a4ebd70a22e903a9e23b6
Shinu <shrinivasbakale2002@gmail.com>
2024-06-24 21:43:44 +0530

    Navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
-// import "./style.css";
 import { GoHeartFill } from "react-icons/go";
 import MainIcon from "../assets/Main Icon.png";
-// import { IoClose } from "react-icons/io5";
-// import { IoMenu } from "react-icons/io5";
 import { IoMdSearch, IoMdHome } from "react-icons/io";
 import { RiDiscountPercentFill } from "react-icons/ri";
 import { HiViewGrid } from "react-icons/hi";
 import { FaShoppingCart } from "react-icons/fa";
-import { FaUser } from "react-icons/fa";
-import { FaBoxOpen } from "react-icons/fa";
+import { FaUser, FaBoxOpen } from "react-icons/fa";
 import Logout from "../assets/Logout.png";
 
 function Navbar() {
@@ -41,8 +37,8 @@ function Navbar() {
 
   return (
     <>
-      <div className=" ">
-        <nav className=" border-gray-200 bg-black  z-20">
+      <div className="">
+        <nav className="border-gray-200 bg-black z-20">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <Link
               to="/"
@@ -53,10 +49,10 @@ function Navbar() {
             <div className="flex items-center justify-between md:order-2">
               <button
                 type="button"
-                data-collapse-toggle="navbar-search"
                 aria-controls="navbar-search"
-                aria-expanded="false"
-                className="md:hidden text-gray-500  hover:bg-gray-700 focus:outline-none focus:ring-2  focus:ring-gray-500 rounded-lg text-sm p-2.5 me-1"
+                aria-expanded={isMenuOpen ? "true" : "false"}
+                className="md:hidden text-gray-500 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 rounded-lg text-sm p-2.5 me-1"
+                onClick={handleMenuToggle}
               >
                 <IoMdSearch className="text-gray-500" size={30} />
                 <span className="sr-only">Search</span>
@@ -68,22 +64,19 @@ function Navbar() {
                 <input
                   type="text"
                   id="search-navbar"
-                  className="block w-full p-2 ps-10 text-sm text-gray-900 border  rounded-lg  focus:ring-blue-500 focus:border-blue-500 bg-white border-gray-600 placeholder-gray-400"
+                  className="block w-full p-2 ps-10 text-sm text-gray-900 border rounded-lg focus:ring-blue-500 focus:border-blue-500 bg-white border-gray-600 placeholder-gray-400"
                   placeholder="Search Products"
                 />
-
                 <Link to="/wishlist">
                   <div className="bg-[#37317C] h-9 w-9 rounded-full flex items-center justify-center">
                     <GoHeartFill className="text-white" />
                   </div>
                 </Link>
-
                 <Link to="/cart">
                   <div className="bg-[#37317C] h-9 w-9 rounded-full flex items-center justify-center">
                     <FaShoppingCart className="text-white" />
                   </div>
                 </Link>
-
                 <Link
                   className="flex items-center relative"
                   onClick={handleDropdownToggle}
@@ -102,7 +95,7 @@ function Navbar() {
                     } font-normal divide-y divide-gray-100 rounded-md shadow w-44 absolute left-1/2 transform -translate-x-1/2 top-full -ml-24`}
                   >
                     <ul
-                      className="py-2 text-sm bg-white absolute  rounded-xl w-64"
+                      className="py-2 text-sm bg-white absolute rounded-xl w-64"
                       aria-labelledby="dropdownLargeButton"
                     >
                       <div className="flex p-7 justify-start items-center font-semibold border-b border-gray-300 h-10 text-lg w-full">
@@ -111,7 +104,7 @@ function Navbar() {
                       <li className="border-b ">
                         <Link
                           to="/cart"
-                          className="flex items-center gap-4 px-4 py-2 border-b border-gray-100 hover:text-[#C342F9] "
+                          className="flex items-center gap-4 px-4 py-2 border-b border-gray-100 hover:text-[#C342F9]"
                         >
                           <div className="rounded-full p-3 bg-[#FEECFF] text-center">
                             <FaShoppingCart
@@ -122,11 +115,10 @@ function Navbar() {
                           <p className="text-lg">Cart</p>
                         </Link>
                       </li>
-
                       <li className="border-b ">
                         <Link
                           to="/wishlist"
-                          className="flex items-center gap-4 px-4 py-2 border-b border-gray-100 hover:text-[#C342F9] "
+                          className="flex items-center gap-4 px-4 py-2 border-b border-gray-100 hover:text-[#C342F9]"
                         >
                           <div className="rounded-full p-3 bg-[#FEECFF] text-center">
                             <GoHeartFill className="text-[#9747FF]" size={20} />
@@ -137,7 +129,7 @@ function Navbar() {
                       <li className="border-b ">
                         <Link
                           to="/orders"
-                          className="flex items-center gap-4 px-4 py-2 border-b border-gray-100 hover:text-[#C342F9] "
+                          className="flex items-center gap-4 px-4 py-2 border-b border-gray-100 hover:text-[#C342F9]"
                         >
                           <div className="rounded-full p-3 bg-[#EFFAFE] text-center">
                             <FaBoxOpen className="text-[#3366FF]" size={20} />
@@ -148,7 +140,7 @@ function Navbar() {
                       <li className="border-b ">
                         <Link
                           to="/profile"
-                          className="flex items-center gap-4 px-4 py-2 border-b border-gray-100 hover:text-[#C342F9] "
+                          className="flex items-center gap-4 px-4 py-2 border-b border-gray-100 hover:text-[#C342F9]"
                         >
                           <div className="rounded-full p-3 bg-[#EFFAFE] text-center">
                             <FaUser className="text-[#5356FB]" size={20} />
@@ -159,7 +151,7 @@ function Navbar() {
                       <li className="border-b ">
                         <a
                           href="#"
-                          className="flex items-center gap-4 px-4 py-2  hover:text-[#C342F9] "
+                          className="flex items-center gap-4 px-4 py-2 hover:text-[#C342F9]"
                         >
                           <div className="rounded-full p-2 bg-[#FDEFEF] text-center">
                             <img src={Logout} />
@@ -172,11 +164,11 @@ function Navbar() {
                 </Link>
               </div>
               <button
-                data-collapse-toggle="navbar-search"
                 type="button"
-                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden  focus:outline-none focus:ring-2  hover:bg-gray-700 focus:ring-gray-600"
+                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none focus:ring-2 hover:bg-gray-700 focus:ring-gray-600"
                 aria-controls="navbar-search"
-                aria-expanded="false"
+                aria-expanded={isMenuOpen ? "true" : "false"}
+                onClick={handleMenuToggle}
               >
                 <span className="sr-only">Open main menu</span>
                 <svg
@@ -188,16 +180,18 @@ function Navbar() {
                 >
                   <path
                     stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M1 1h15M1 7h15M1 13h15"
                   />
                 </svg>
               </button>
             </div>
             <div
-              className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+              className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+                isMenuOpen ? "block" : "hidden"
+              }`}
               id="navbar-search"
             >
               <div className="relative mt-3 md:hidden">
@@ -211,9 +205,9 @@ function Navbar() {
                   >
                     <path
                       stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                     />
                   </svg>
@@ -221,11 +215,11 @@ function Navbar() {
                 <input
                   type="text"
                   id="search-navbar"
-                  className="block w-full p-2 ps-10 text-sm text-gray-900 border  rounded-lg   focus:border-blue-500 bg-gray-700 border-gray-600 placeholder-gray-400  focus:ring-blue-500"
+                  className="block w-full p-2 ps-10 text-sm text-gray-900 border rounded-lg focus:border-blue-500 bg-gray-700 border-gray-600 placeholder-gray-400 focus:ring-blue-500"
                   placeholder="Search..."
                 />
               </div>
-              <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 bg-black  border-gray-700">
+              <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 bg-black border-gray-700">
                 <li className="group hover:text-[#F8A926]">
                   <Link
                     to="/"
@@ -239,14 +233,13 @@ function Navbar() {
                     Home
                   </Link>
                 </li>
-
                 <li className="hover:text-[#F8A926]">
                   <Link
                     to="/schemes"
-                    className="py-2 px-3 rounded md:p-0 text-white  hover:text-[#F8A926] flex gap-2"
+                    className="py-2 px-3 rounded md:p-0 text-white hover:text-[#F8A926] flex gap-2"
                   >
                     <RiDiscountPercentFill
-                      className="hover:text-[#F8A926] "
+                      className="hover:text-[#F8A926]"
                       size={25}
                     />
                     Schemes
@@ -256,14 +249,14 @@ function Navbar() {
                   <button
                     id="dropdownNavbarLink"
                     data-dropdown-toggle="dropdownNavbar"
-                    className="flex items-center w-full gap-2 py-2 px-3 hover:text-[#F8A926] md:hover:bg-transparent md:border-0 md:hover:text-[#F8A926] md:p-0 md:w-auto text-white  focus:text-white hover:bg-gray-700 "
+                    className="flex items-center w-full gap-2 py-2 px-3 hover:text-[#F8A926] md:hover:bg-transparent md:border-0 md:hover:text-[#F8A926] md:p-0 md:w-auto text-white focus:text-white hover:bg-gray-700"
                   >
                     <HiViewGrid size={25} />
                     All Categories <span className="text-xl">▾</span>
                   </button>
                   <div
                     id="dropdownNavbar"
-                    className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 "
+                    className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44"
                   >
                     <ul
                       className="py-2 text-sm text-gray-700 font-semibold"
@@ -272,7 +265,7 @@ function Navbar() {
                       <li>
                         <a
                           href="#"
-                          className="block px-4 py-2  hover:text-[#C342F9]"
+                          className="block px-4 py-2 hover:text-[#C342F9]"
                         >
                           Category 1
                         </a>
@@ -280,7 +273,7 @@ function Navbar() {
                       <li>
                         <a
                           href="#"
-                          className="block px-4 py-2  hover:text-[#C342F9]"
+                          className="block px-4 py-2 hover:text-[#C342F9]"
                         >
                           Category 2
                         </a>
