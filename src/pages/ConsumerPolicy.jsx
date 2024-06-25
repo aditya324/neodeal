2e0edff37b65b88fe17096ef6b2040151750ab5f
Saishree L <saishreegit@gmail.com>
2024-06-25 18:22:49 +0530

    about us

---
+++ src/pages/ConsumerPolicy.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { FaArrowLeftLong } from 'react-icons/fa6'
+
+const ConsumerPolicy = () => {
+  return (
+    <div className="bg-gray-300 min-h-screen xl:px-32 px-3 py-5">
+        <div className="mt-5 flex">
+        <FaArrowLeftLong className="text-2xl" />
+        <p className='ml-2 text-xl font-semibold'>Consumer policy</p>
+      </div>
+      <div className='text-center mt-5'>
+        <p className='font-semibold text-2xl text-[#5356FB]'>Consumer Policy</p>
+        <p className='text-textGrey text-base font-normal lg:leading-8 mt-5'>Neodeals is Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with</p>
+      </div>
+      <div className='text-center mt-10'>
+        <p className='font-semibold text-2xl text-[#5356FB]'>Cancellation policy</p>
+        <p className='text-textGrey text-base font-normal lg:leading-8 mt-5'>Neodeals is Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default ConsumerPolicy
