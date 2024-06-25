11c40e89fade4b8ecc993fbe3dbcf3488512c4f2
Abdul Qayyum Mulla <abdulqayyummulla@Abduls-Air.lan>
2024-06-25 21:21:10 +0530

    neo merge new

--- src/App.jsx
+++ src/App.jsx
@@ -12,24 +12,24 @@ import LoginEmail from "./pages/LoginEmail";
 import LoginOtp from "./pages/LoginOtp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Orders from "./pages/Orders";
-import Address from "./pages/Address"; 
+import Address from "./pages/Address";
 import Payment from "./pages/Payment";
 import Cart from "./pages/Cart";
 import Categories from "./pages/Categories";
 import Wishlist from "./pages/Wishlist";
 import Profile from "./pages/Profile";
-import Product from './pages/Product'
-import PersonalInfo from './pages/PersonalInfo';
-import Reviews from './pages/Reviews'
-import YourScheme from './pages/YourScheme';
-import Navbar from './components/Navbar';
-import Spin from './pages/Spin';
+import Product from "./pages/Product";
+import PersonalInfo from "./pages/PersonalInfo";
+import Reviews from "./pages/Reviews";
+import YourScheme from "./pages/YourScheme";
+import Navbar from "./components/Navbar";
+import Spin from "./pages/Spin";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout/>}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="schemes" element={<SchemesPage />} />
           <Route path="login" element={<Login />} />
@@ -46,11 +46,11 @@ function App() {
           <Route path="categories" element={<Categories />} />
           <Route path="wishlist" element={<Wishlist />} />
           <Route path="profile" element={<Profile />} />
-          <Route path='productPage' element={<Product/>}/>
-          <Route path='personalInfo' element={<PersonalInfo/>}/>
-          <Route path='reviews' element={<Reviews/>}/>
-          <Route path='yourScheme' element={<YourScheme/>}/>
-          <Route path='spin' element={<Spin/>}/>
+          <Route path="productPage" element={<Product />} />
+          <Route path="personalInfo" element={<PersonalInfo />} />
+          <Route path="reviews" element={<Reviews />} />
+          <Route path="yourScheme" element={<YourScheme />} />
+          <Route path="spin" element={<Spin />} />
         </Route>
       </Routes>
     </Router>
