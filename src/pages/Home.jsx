09aac30425a4ac91742f7fa48e8c8141e664ce60
aditya <adityakulkarni54321@gmail.com>
2024-06-25 17:01:02 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,82 +13,84 @@ import PopularCategories from "../components/PopularCategories";
 
 const Home = () => {
 
-  
+
 
   return (
     <>
-     <div>
-     <div className="bg-custom-gradient w-full h-auto lg:h-96 flex flex-col lg:flex-row justify-start items-center gap-16">
-  <div
-    className="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 p-5 rounded-2xl lg:h-auto   place-items-center w-full lg:w-4/5 md:w-5/6 lg:ml-12 md:ml-0 "
-    style={{ backgroundImage: `url(${purbbg})`,  backgroundSize: "cover",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat" }}
-  >
-    <div className="">
-      <p className="text-white font-semibold text-lg">Buy any product from schemes and</p>
-      <p className="text-white text-7xl font-extrabold my-2">WIN BIG!</p>
-      <p className="text-white font-medium">
-        Each purchase of a product earns you a unique number,
-        unlocking the chance to <br /> win lavish prizes. 
-      </p>
-      <div className="flex  gap-10 justify-start items-center">
-        <button className="bg-[#F539F8] text-white my-5 px-8 py-3 rounded-full">
-          Buy Now
-        </button>
-        <a href="" className="text-white underline">
-          Learn More
-        </a>
-      </div>
-    </div>
-    <div>
-      <img src={winnbg} alt="" className="w-full h-auto " />
-      <div className="text-center font-semibold text-white">
-        <p>
-        Its as simple as.. Buy a product,<br />
-        spin
-spin the wheel & WIN!
-        </p>
-      </div>
-    </div>
+      <div>
+        <div className="bg-custom-gradient w-full h-auto lg:h-96 flex flex-col lg:flex-row justify-start items-center gap-3 lg:px-7">
+          <div
+            className="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 p-5 rounded-2xl lg:h-auto   place-items-center w-full lg:w-5/6 md:w-5/6 lg:ml-12 md:ml-0 lg:px-10 "
+            style={{
+              backgroundImage: `url(${purbbg})`, backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat"
+            }}
+          >
+            <div className="">
+              <p className="text-white font-semibold text-lg">Buy any product from schemes and</p>
+              <p className="text-white text-7xl font-extrabold my-2">WIN BIG!</p>
+              <p className="text-white font-medium">
+                Each purchase of a product earns you a unique number,
+                unlocking the chance to <br /> win lavish prizes.
+              </p>
+              <div className="flex  gap-10 justify-start items-center">
+                <button className="bg-[#F539F8] text-white my-5 px-8 py-3 rounded-full">
+                  Buy Now
+                </button>
+                <a href="" className="text-white underline">
+                  Learn More
+                </a>
+              </div>
+            </div>
+            <div>
+              <img src={winnbg} alt="" className="w-full h-auto " />
+              <div className="text-center font-semibold text-white">
+                <p>
+                  Its as simple as.. Buy a product,<br />
+                  spin
+                  spin the wheel & WIN!
+                </p>
+              </div>
+            </div>
 
-  </div>
-  <div className="lg:mx-0 mx-10 lg:mb-0 mb-10 ">
-  <div className="w-full  h-auto lg:h-80 rounded-xl lg:py-0 py-5 lg:mr-16 md:mr-0 mr-0   "
-  style={{backgroundImage:`url(${yelimg})`,width:"380px"}}>
-    <div className="flex flex-col items-center lg:py-10">
-      <p className="text-red-600 text-lg font-semibold">Your ticket to luxury!</p>
-      <p className="text-indigo-600 font-bold text-xl">Introducing</p>
-      <h1 className="text-indigo-600 text-6xl font-bold">Schemes</h1>
-      <p className=" font-bold text-center ">Shop for products starting at just <br /> ₹299 and win incredible prizes.</p>
-      
-      {/* <button className="bg-[#5356FB] hover:bg-blue-600 text-white font-bold py-2 px-20 rounded-full mt-6">
+          </div>
+          <div className="lg:mx-0 mx-10 lg:mb-0 mb-10 lg:px-5 ">
+            <div className="w-80  h-auto lg:h-80 rounded-xl lg:py-0 py-5 lg:mr-16 md:mr-0 mr-0  lg:px-5   "
+              style={{ backgroundImage: `url(${yelimg})` }}>
+              <div className="flex flex-col items-center lg:py-10">
+                <p className="text-red-600 text-lg font-semibold">Your ticket to luxury!</p>
+                <p className="text-indigo-600 font-bold text-xl">Introducing</p>
+                <h1 className="text-indigo-600 text-6xl font-bold">Schemes</h1>
+                <p className=" font-bold text-center ">Shop for products starting at just <br /> ₹299 and win incredible prizes.</p>
+
+                {/* <button className="bg-[#5356FB] hover:bg-blue-600 text-white font-bold py-2 px-20 rounded-full mt-6">
           Submit
       </button> */}
-      <Button name="VIew All Schemes" style="bg-[#5356FB] hover:bg-blue-600 text-white font-bold py-3 text- px-10 rounded-full mt-6" />
-      
-    </div>
-  </div>
-  </div>
-</div>
+                <Button name="VIew All Schemes" style="bg-[#5356FB] hover:bg-blue-600 text-white font-bold py-3 text- px-10 rounded-full mt-6" />
+
+              </div>
+            </div>
+          </div>
+        </div>
 
 
 
-   
-
-<PurpleSlider/>
 
 
-<BestSellerCard name="Best Seller" style="font-semibold text-[#374557] text-xl lg:ml-20 ml-5  mb-5 text-xl" />
+        <PurpleSlider />
 
 
-<PopularCategories/>
+        <BestSellerCard name="Best Seller" style="font-semibold text-[#374557] text-xl text-xl" className='lg:mt-10 '/>
+
+
+        <PopularCategories />
 
 
 
-     
-     </div>
-    
+
+      </div>
+
 
     </>
   );
